9d0fea246fe0be8c0aa0f4567defcba67589716e
TheNorthForty <matt.l.armstrong@gmail.com>
2020-05-22 17:26:56 -0500

    edit and new functions

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,4 +8,36 @@ class ArticlesController < ApplicationController
   def index
     @articles = Article.all
   end
+
+  def new
+    @article = Article.new # creates empty article to prevent errors in new.html.erb error check
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def create
+    # must whitelist what will be allowed to interact with your code
+    @article = Article.new(params.require(:article).permit(:title, :description))
+    if @article.save
+      flash[:notice] = "Article was created successfully"
+      #redirect_to article_path(@article)
+      redirect_to @article # synsugar
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    # must whitelist what will be allowed to interact with your code
+    if @article.update(params.require(:article).permit(:title, :description))
+      flash[:notice] = "Article was updated successfully."
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
+  
 end
