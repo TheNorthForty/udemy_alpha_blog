93ad904c79a003f7e3ea20cb895ef3179fa4af36
TheNorthForty <matt.l.armstrong@gmail.com>
2020-05-22 10:54:00 -0500

    add articles listing feature

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root 'pages#home'
   get 'about', to: 'pages#about'
-  resources :articles, only: [:show] # restrict resource creation to show
+  resources :articles, only: [:show, :index] # restrict resource creation to show
 end
