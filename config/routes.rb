5fb1bba806fc40b440a132588fcb651f96f457e1
TheNorthForty <matt.l.armstrong@gmail.com>
2020-05-29 17:19:58 -0500

    new user signup

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   get 'about', to: 'pages#about'
   # restrict resource creation to show
   resources :articles #, only: [:show, :index, :new, :create, :edit, :update, :destroy] 
+  get 'signup', to: 'users#new'
+  resources :users, except: [:new]
 end
