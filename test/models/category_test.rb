fbfede625c987ebf69b57434f4dce852ae48848a
TheNorthForty <matt.l.armstrong@gmail.com>
2020-07-01 06:34:28 -0500

    Category model, testing, validation, controller

--- test/models/category_test.rb
+++ test/models/category_test.rb
@@ -2,9 +2,37 @@ require 'test_helper'
 
 class CategoryTest < ActiveSupport::TestCase
 
-  test "category should be valid" do
+  def setup
     @category = Category.new(name: "Sports")
+  end
+
+  test "category should be valid" do
+    # @category = Category.new(name: "Sports")
+    # test items self-destruct, so @category isn't available
+    # after each test is run. Putting this in "setup" allows 
+    # that to run before each test.
     assert @category.valid?
+  end
+
+  test "name should be present" do
+    @category.name = " "
+    assert_not @category.valid?
+  end
+
+  test "name should be unique" do
+    @category.save
+    @category2 = Category.new(name: "Sports")
+    assert_not @category2.valid?
+  end
+
+  test "name should not be too long" do
+    @category.name = "a" * 26
+    assert_not @category.valid?
+  end
+
+  test "name should not be too short" do
+    @category.name = "aa"
+    assert_not @category.valid?
   end
   
 end
